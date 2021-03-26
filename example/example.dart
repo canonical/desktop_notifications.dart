63788d74ad8c050cd16dfb3a6558d672c2eac029
Robert Ancell <robert.ancell@canonical.com>
2021-03-26 15:29:24 +1300

    Rename NotificationClient to NotificationsClient (was a typo).

--- example/example.dart
+++ example/example.dart
@@ -1,7 +1,7 @@
 import 'package:desktop_notifications/desktop_notifications.dart';
 
 void main() async {
-  var client = NotificationClient();
+  var client = NotificationsClient();
   await client.notify('Hello World!');
   await client.close();
 }
