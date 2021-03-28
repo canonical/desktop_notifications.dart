f0f54806a179917172b793b1e7a0b44daeb37616
Robert Ancell <robert.ancell@canonical.com>
2021-03-29 10:47:02 +1300

    Add an example of closing your own notification

--- example/close.dart
+++ example/close.dart
@@ -2,7 +2,11 @@ import 'package:desktop_notifications/desktop_notifications.dart';
 
 void main() async {
   var client = NotificationsClient();
-  var notification = await client.notify('Close Me!', expireTimeoutMs: 5000);
-  print('Notification closed due to reason ${await notification.closeReason}');
+
+  // Close our own notification after one second.
+  var notification = await client.notify('Changed my mind');
+  await Future.delayed(Duration(seconds: 1));
+  await notification.close();
+
   await client.close();
 }
